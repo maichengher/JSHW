cffbadfdca05515459dbdaeb4a1bdd25eacf2145
Mai Cheng Her <50380028+maichengher@users.noreply.github.com>
2019-05-12 20:54:51 -0500

    Add files via upload

---
+++ JSHW/review.js
@@ -0,0 +1,19 @@
+var string = 'this is what a string looks like';
+var number = 25;
+var boolean = true;
+console.log(string);
+console.log(number);
+console.log(boolean);
+var equation = 35+2;
+var equation2 = equation+99;
+var taste = ['sour', 'spicy', 'bitter', 'sweet'];
+taste.push('tart');
+console.log(taste.splice(1,3));
+console.log(taste);
+taste.reverse();
+console.log(taste);
+var husband ="Touly"
+var husbandsentence = `My husband's name is ${husband}`;
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof boolean);
